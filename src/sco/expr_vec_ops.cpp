0ab18d611532e9feabc657177d04938d02efc79c
Chelsea Zhang <zhang.chelsea@gmail.com>
2016-12-05 21:35:26 -0800

    implement exprDot

--- src/sco/expr_vec_ops.cpp
+++ src/sco/expr_vec_ops.cpp
@@ -1,3 +1,4 @@
+#include "sco/expr_ops.hpp"
 #include "expr_vec_ops.hpp"
 namespace sco {
 
@@ -7,6 +8,15 @@ AffExpr varDot(const VectorXd& x, const VarVector& v) {
   out.constant = 0;
   out.vars = v;
   out.coeffs = vector<double>(x.data(), x.data()+x.size());
+  return out;
+}
+
+AffExpr exprDot(const VectorXd& x, const AffExprVector& v) {
+  // probably not the most efficient implementation
+  AffExpr out;
+  for (int i=0; i < x.size(); ++i) {
+    exprInc(out, exprMult(v[i], x[i]));
+  }
   return out;
 }
 
